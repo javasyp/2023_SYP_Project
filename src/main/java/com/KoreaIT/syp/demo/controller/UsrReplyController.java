91f563bd6016c0c0eaae80aeff62dee442a8ebfc
javasyp <tlsdud625@naver.com>
2023-05-02 17:00:49 +0900

    댓글 작성 구현

--- src/main/java/com/KoreaIT/syp/demo/controller/UsrReplyController.java
+++ src/main/java/com/KoreaIT/syp/demo/controller/UsrReplyController.java
@@ -1,31 +1,47 @@
-//package com.KoreaIT.syp.demo.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import com.KoreaIT.syp.demo.service.ReplyService;
-//import com.KoreaIT.syp.demo.util.Ut;
-//import com.KoreaIT.syp.demo.vo.Member;
-//import com.KoreaIT.syp.demo.vo.ResultData;
-//import com.KoreaIT.syp.demo.vo.Rq;
-//
-//@Controller
-//public class UsrReplyController {
-//	
-//	@Autowired
-//	private Rq rq;
-//	@Autowired
-//	private ReplyService replyService;
-//	
-//	// 회원가입
-//	@RequestMapping("/usr/reply/doWrite")
-//	@ResponseBody
-//	public String doJoin(String loginId, String loginPw,
-//			String name, String nickname, String cellphoneNum, String email) {
-//
-//
-//		return "";
-//	}
-//}
+package com.KoreaIT.syp.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.KoreaIT.syp.demo.service.ReplyService;
+import com.KoreaIT.syp.demo.util.Ut;
+import com.KoreaIT.syp.demo.vo.ResultData;
+import com.KoreaIT.syp.demo.vo.Rq;
+
+@Controller
+public class UsrReplyController {
+	
+	@Autowired
+	private Rq rq;
+	@Autowired
+	private ReplyService replyService;
+	
+	@RequestMapping("/usr/reply/doWrite")
+	@ResponseBody
+	public String doWrite(String relTypeCode, int relId, String body, String replaceUri) {
+
+		if (Ut.empty(relTypeCode)) {
+			return rq.jsHistoryBack("F-1", "relTypeCode 을(를) 입력해 주세요.");
+		}
+		
+		if (Ut.empty(relId)) {
+			return rq.jsHistoryBack("F-2", "relId 을(를) 입력해 주세요.");
+		}
+		
+		if (Ut.empty(body)) {
+			return rq.jsHistoryBack("F-3", "body 을(를) 입력해 주세요.");
+		}
+
+		ResultData<Integer> writeReplyRd = replyService.writeReply(rq.getLoginedMemberId(), relTypeCode, relId, body);
+
+		int id = (int) writeReplyRd.getData1();
+
+		if (Ut.empty(replaceUri)) {
+			replaceUri = Ut.f("../article/detail?id=%d", relId);
+		}
+
+		return rq.jsReplace(writeReplyRd.getMsg(), replaceUri);
+	}
+}
