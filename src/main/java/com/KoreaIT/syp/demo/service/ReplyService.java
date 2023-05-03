a3ef76d21400ca23b9f520ce6a6238e1f631e69d
javasyp <tlsdud625@naver.com>
2023-05-03 14:38:15 +0900

    댓글 수정 및 삭제 버튼 노출

--- src/main/java/com/KoreaIT/syp/demo/service/ReplyService.java
+++ src/main/java/com/KoreaIT/syp/demo/service/ReplyService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.KoreaIT.syp.demo.vo.Article;
 import com.KoreaIT.syp.demo.vo.Reply;
 import com.KoreaIT.syp.demo.repository.ReplyRepository;
 import com.KoreaIT.syp.demo.util.Ut;
@@ -32,8 +33,48 @@ public class ReplyService {
 	
 	// 댓글 목록
 	public List<Reply> getForPrintReplies(int actorId, String relTypeCode, int relId) {
+		List<Reply> replies = replyRepository.getForPrintReplies(actorId, relTypeCode, relId);
 		
-		return replyRepository.getForPrintReplies(actorId, relTypeCode, relId);
+		for (Reply reply : replies) {
+			controlForPrintData(actorId, reply);
+		}
+		
+		return replies;
+	}
+	
+	// 댓글 수정 및 삭제 버튼 노출 여부
+	private void controlForPrintData(int actorId, Reply reply) {
+		if (reply == null) {
+			return;
+		}
+
+		ResultData actorCanDeleteRd = actorCanDelete(actorId, reply);
+		reply.setActorCanDelete(actorCanDeleteRd.isSuccess());
+		
+		ResultData actorCanModifyRd = actorCanModify(actorId, reply);
+		reply.setActorCanModify(actorCanModifyRd.isSuccess());
+	}
+	
+	// 댓글 수정 권한 체크
+	public ResultData actorCanModify(int loginedMemberId, Reply reply) {
+		if (reply.getMemberId() != loginedMemberId) {
+			return ResultData.from("F-2", Ut.f("해당 댓글에 대한 권한이 없습니다."));
+		}
+		
+		return ResultData.from("S-1", "수정 가능");
+	}
+	
+	// 댓글 삭제 권한 체크
+	private ResultData actorCanDelete(int actorId, Reply reply) {
+		if (reply == null) {
+			return ResultData.from("F-1", "댓글이 존재하지 않습니다.");
+		}
+
+		if (reply.getMemberId() != actorId) {
+			return ResultData.from("F-2", "해당 댓글에 대한 권한이 없습니다.");
+		}
+
+		return ResultData.from("S-1", "삭제 가능");
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/usr/article/detail.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/article/detail.jsp
@@ -196,6 +196,8 @@
 				<col width="100" />
 				<col width="50" />
 				<col width="150" />
+				<col width="50" />
+				<col width="50" />
 			</colgroup>
 			<thead>
 				<tr>
@@ -204,6 +206,8 @@
 					<th>작성자</th>
 					<th>추천</th>
 					<th>내용</th>
+					<th>수정</th>
+					<th>삭제</th>
 				</tr>
 			</thead>
 
@@ -217,6 +221,17 @@
 						<td>${reply.extra_writer}</td>
 						<td>${reply.goodReactionPoint}</td>
 						<td align="left">${reply.body}</td>
+						<td>
+							<c:if test="${reply.actorCanModify }">
+								<a class="btn-text-link btn btn-active btn-ghost" href="../reply/modify?id=${reply.id }">수정</a>
+							</c:if>
+						</td>
+						<td>
+						<c:if test="${reply.actorCanModify }">
+								<a class="btn-text-link btn btn-active btn-ghost" onclick="if(confirm('정말 삭제하시겠습니까?') == false) return false;"
+									href="../reply/doDelete?id=${reply.id }">삭제</a>
+							</c:if>
+						</td>
 					</tr>
 				</c:forEach>
 			</tbody>
